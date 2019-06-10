5df88a464a2182f9203fa6b474ffcd8a0991186c
Christian.Kohl <christianalbert.k@gmail.com>
2019-06-10 22:48:09 +0100

    Working data entry, next steps are to add comments

--- diar.py
+++ diar.py
@@ -2,7 +2,7 @@ import click
 import numpy as np
 from pathlib import Path
 import os
-import sqlite3 as sql
+import sqlite3
 import datetime
 
 diarname = 'diarpy.db'
@@ -10,7 +10,7 @@ diarname = 'diarpy.db'
 def connect_db():
     conn = sqlite3.connect(diarname)
     c = conn.cursor()
-    return c
+    return conn, c
 
 def setup_db(c):
     c.execute('''CREATE TABLE entry (
@@ -21,25 +21,34 @@ def setup_db(c):
               )''')
 
 def add_entry(c, date, title, text):
+    print(type(date))
+    print(type(title))
+    text = ' '.join(text)
+    print(type(text))
     c.execute("INSERT INTO entry (date, title, entry) VALUES (?, ?, ?)",
-              (date, title, text))
+              (date, title, text, ))
 
 @click.command()
+@click.option('--get', default=False)
 @click.argument('text', required=True, nargs=-1)
-def diar(text):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    file_path = dir_path + '/' + diarname
-    exist = os.path.exists(file_path)
-    cur = connect_db()
-    date = datetime.datetime.now()
-    if exist:
-        cur = connect_db()
-        add_entry(cur, date, 'aaa', text)
+def diar(get, text):
+    if get:
+        conn, cur = connect_db()
+        x = cur.execute('SELECT * FROM entry')
     else:
-        click.echo('Your diary will be stored in:' + file_path)
-        cur = connect_db()
-        setup_db(cur)
-        add_entry(cur, date, 'aaa', text)
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        file_path = dir_path + '/' + diarname
+        exist = os.path.exists(file_path)
+        conn, cur = connect_db()
+        date = datetime.datetime.now()
+        if exist:
+            add_entry(cur, date, 'aaa', text)
+        else:
+            click.echo('Your diary will be stored in:' + file_path)
+            setup_db(cur)
+            add_entry(cur, date, 'aaa', text)
+    conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
     diar()
