e30eb9d789649843e5d059b706eaac631d05d828
Christian.Kohl <christianalbert.k@gmail.com>
2019-06-11 15:26:59 +0100

    Added changes, litting everything

--- diar.py
+++ diar.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import os
 import sqlite3
 import datetime
+import dateparser as db
 
 diarname = 'diarpy.db'
 
@@ -20,13 +21,23 @@ def setup_db(c):
               Entry text
               )''')
 
-def add_entry(c, date, title, text):
-    print(type(date))
-    print(type(title))
+def add_entry(c, text):
     text = ' '.join(text)
-    print(type(text))
+    if ':' in text:
+        header = text.split(':')[0]
+        text = text[len(header)+1:]
+        if text[0] == ' ':
+            text = text[1:]
+        date, title = extract_header(header)
+    else:
+        date = datetime.datetime.now().replace(microsecond=0,
+                                               second=0,
+                                               minute=0)
+        title = "Journal Entry"
+    print(header)
+    print(date)
     c.execute("INSERT INTO entry (date, title, entry) VALUES (?, ?, ?)",
-              (date, title, text, ))
+              (date, header, text, ))
 
 @click.command()
 @click.option('--get', default=False)
@@ -40,15 +51,27 @@ def diar(get, text):
         file_path = dir_path + '/' + diarname
         exist = os.path.exists(file_path)
         conn, cur = connect_db()
-        date = datetime.datetime.now()
         if exist:
-            add_entry(cur, date, 'aaa', text)
+            add_entry(cur, text)
         else:
             click.echo('Your diary will be stored in:' + file_path)
             setup_db(cur)
             add_entry(cur, date, 'aaa', text)
     conn.commit()
     conn.close()
+
+def extract_header(header):
+    sections = header.split(',')
+    if len(sections) > 2:
+        click.echo("Sorry, you can't have commas in your title, please try again")
+    elif len(sections) == 1:
+        date = db.parse(sections[0])
+        if date is None:
+            date = datetime.datetime.now().replace(microsecond=0,
+                                                   second=0,
+                                                   minute=0)
+    else:
+        return datetime.datetime.now(), 'Hello'
 
 if __name__ == '__main__':
     diar()
